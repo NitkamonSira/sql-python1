6f1192a684c6a93949147b812198a4c9ef03c2b0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-30 13:25:13 +1200

    add code

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import sqlite3
 db = sqlite3.connect('something.db')
-cursor = 
+cursor = db.cursor()
+sql = "SELECT * FROM Colour;"
+cursor.execute(sql)
+results = cursor.fetchall()
+# print nicely
+for colour in results:
+    print(colour)
+
+# close the db
+db.close()
